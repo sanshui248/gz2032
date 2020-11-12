2693d4d461346cdaa69139370b4677c9c7f60d5e
zs <123@qq.com>
2020-11-12 16:14:31 +0800

    再次测试

--- TestProject/src/com/gec/TestDemo.java
+++ TestProject/src/com/gec/TestDemo.java
@@ -1,6 +1,10 @@
 package com.gec;
 
 public class TestDemo {
+	static String name;
+	public static String getName() {
+		return name;
+	}
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 	}
