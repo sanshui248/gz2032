ca55f36fcdc31ae7ec9e853ad3446997974b2d28
Administrator <Administrator@PC50>
2020-11-13 10:24:07 +0800

    志勇提交

--- TestProject/src/com/gec/TestDemo.java
+++ TestProject/src/com/gec/TestDemo.java
@@ -2,15 +2,23 @@ package com.gec;
 
 public class TestDemo {
 	static String name;
+	int age;
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
 	public static String getName() {
 		return name;
 	}
-	
-	
+
 	public static void setName(String name) {
 		TestDemo.name = name;
 	}
-
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
