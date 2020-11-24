9457950b50d45df910ffd35870f31654aff989a2
Akron-xz <502793920@qq.com>
2020-11-24 21:09:58 +0800

    公告栏时间字符串

--- LibraryMS/src/main/java/com/ibm/domain/Announce.java
+++ LibraryMS/src/main/java/com/ibm/domain/Announce.java
@@ -1,5 +1,6 @@
 package com.ibm.domain;
 
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -10,6 +11,7 @@ import java.util.Date;
 public class Announce {
 	private  int announceId;         //公告id
 	private  Date announceTime;      //公告日期
+	private String announceTimeStr;
 	private  String announceContent;  //公告内容
 	public int getAnnounceID() {
 		return announceId;
@@ -22,6 +24,23 @@ public class Announce {
 	}
 	public void setAnnounceTime(Date announceTime) {
 		this.announceTime = announceTime;
+		this.setAnnounceTimeStr();
+	}
+	public int getAnnounceId() {
+		return announceId;
+	}
+	public void setAnnounceId(int announceId) {
+		this.announceId = announceId;
+	}
+	public String getAnnounceTimeStr() {
+		return announceTimeStr;
+	}
+	public void setAnnounceTimeStr() {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(announceTime);
+		String str = calendar.get(Calendar.YEAR) + "-" + calendar.get(Calendar.MONTH) + "-"
+				+ calendar.get(Calendar.DATE);
+		this.announceTimeStr = str;
 	}
 	public String getAnnounceContent() {
 		return announceContent;
